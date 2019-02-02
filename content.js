8c4c4e34250709b8966fd32c8a61ab8565e2e575
catherinewu12 <wucatherine12@gmail.com>
2019-02-02 14:25:48 -0500

    FIXED

--- content.js
+++ content.js
@@ -2,10 +2,12 @@
 chrome.runtime.onMessage.addListener(function (request, sender,
 	sendResponse) {
 
-	const re = new RegExp('bear','gi')
+	const re = new RegExp(request.inputText,'gi')
 	const matches = document.documentElement.textContent.match(re)
+	
+	console.log(matches.length)
 	sendResponse({count: matches.length})
 	
 })
 
-//look for the input movie name instead of bears
+//look for the input movie name
--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
 	document.querySelector('button').addEventListener('click',
 		onclick, false)
@@ -7,7 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		//const movie = document.getElementById('txt').value;
 		chrome.tabs.query({currentWindow: true, active: true},
 		function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, "Blocking Spoilers", 
+			//get the string value of the input
+			let x = document.getElementById("movie")
+			let inputText = x.value
+
+			//send the input string to the content.js 
+			chrome.tabs.sendMessage(tabs[0].id, {inputText: inputText}, 
 				setCount)
 		})
 	}
@@ -15,48 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	function setCount(res) {
 		const div = document.createElement('div')
 
-		div.textContent = `${res.count} bears`
+		div.textContent = `${res.count} occurrences`
 		document.body.appendChild(div)
-	}
-}, false)
-/*
-
-document.addEventListener('DOMContentLoaded', ()=> {
-	const movie = document.getElementById('movie')
-	window.addEventListener('DOMContentLoaded', function(){
-		chrome.tabs.query({currentWindow: true, active: true},
-			function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {}, setCount)
-			}
-	})
-	}
 	
-	document.querySelector('button').addEventListener('click',
-		onclick, false)
-
-	function onclick() {
-		//const movie = document.getElementById('txt').value;
-		//chrome.runtime.sendMessage()
-		//var movie = document.getElementById('movie')
-		chrome.tabs.query({currentWindow: true, active: true},
-		function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, "Blocking Spoilers"){
-				file: "contents.js"
-			} function() {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					movie: movie
-				},
-				setCount)
-			
-
-	function setCount(res, movie) {
-		const div = document.createElement('div')
-
-		div.textContent = `${res.count} bears`
-		document.body.appendChild(div)
-	}
-		})
 	}
 }, false)
-//receive the movie name and do stuff 
-*/
