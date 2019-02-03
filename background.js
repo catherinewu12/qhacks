51d5e16f7d7937cedbd54710273fde31ee50690e
catherinewu12 <wucatherine12@gmail.com>
2019-02-03 07:57:44 -0500

    comments and spacing

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
+//Initialize the storage of the keywords from the movie
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({keyword: []}, function() {
-        console.log("The keyword is an array.");
     });
 });
